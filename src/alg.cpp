e295266869c36f91de0379d5265c3db38f74ae86
Denis Suchkov <99133137+PlagueDoctoh@users.noreply.github.com>
2022-03-03 22:08:58 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,42 +1,43 @@
 // Copyright 2022 NNTU-CS
 #include <cstdint>
 #include "alg.h"
-#include math.h
 
 double pown(double value, uint16_t n) {
-  if (n == 0)
+  if (n == 0) {
     return 1;
-  else if (n == 1)
+  } else if (n == 1) {
     return value;
-  else if (n > 1)
-    return pow(value, n);
+  } else if (n > 1) {
+    return pown(value, n);
+  }
 }
 
 uint64_t fact(uint16_t n) {
-  fac=1
+  uint64_t fac=1
   if (n > 1) {
-    for (int i = 1, i <= n; i++)
+    for (int i = 1; i <= n; i++)
       fac *= i;
     return fac;
-  }
-  else
+  } else {
     return 1;
+  }
 }
 
 double calcItem(double x, uint16_t n) {
-  return pow(x, n)/fact(n);
+  return pown(x, n)/fact(n);
 }
 
 double expn(double x, uint16_t count) {
   double e = 1;
-  for (int i = 1, i <= count, i++)
-    e+=calcItem(x, i);
+  for (int j = 1; j <= count; j++) {
+    e+=calcItem(x, j);
+  }
   return e;
 }
 
 double sinn(double x, uint16_t count) {
   double sinx=x;
-  for (i = count, i > 0, i--){
+  for (m = count; m > 0; m--) {
     if (count%2 != 0) {
       sinx -= calcItem(x, count);
     }
@@ -46,7 +47,7 @@ double sinn(double x, uint16_t count) {
 
 double cosn(double x, uint16_t count) {
   double cosx=1;
-  for (i = count, i > 0, i--){
+  for (k = count; k > 0; k--) {
     if (count%2 == 0) {
       cosx -= calcItem(x, count);
     }
